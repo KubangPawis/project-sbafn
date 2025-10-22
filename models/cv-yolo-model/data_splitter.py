5646d7d168c6bcc02184aba5c6abcaee2130b6c7
KubangPawis <xanderfernandezperegrin@gmail.com>
2025-10-22 22:26:17 +0800

    feat: add shard manifest formatting function for Label Studio

---
+++ models/cv-yolo-model/data_splitter.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import pandas as pd
+
+# -------------------
+
+REPO_ROOT = Path(__file__).resolve().parents[2]
+SHARD_NUMBER = 1
+
+# -------------------
+
+def make_ls_csv(shard_local_csv, out_csv):
+    df = pd.read_csv(shard_local_csv)
+    split = df["split"].str.lower().map({"validation":"val"}).fillna(df["split"].str.lower())
+
+    def add_prefix(p, s):
+        relative_path = f"data/annotation_v1_images/annotation_v1_shard{SHARD_NUMBER}_local"
+        ls_prefix = "/data/local-files/?d=" # needed prefix for LS
+        p = str(p).replace("\\", "/").lstrip("/")
+        if p.startswith(("train/","val/","test/")):
+            return p
+        return f"{ls_prefix}{relative_path}/{s}/{p}"
+
+    df["image"] = [add_prefix(p, s) for p, s in zip(df["image"], split)]
+    df.to_csv(out_csv, index=False)
+
+if __name__ == "__main__":
+    target_file = f"annotation_v1_shard{SHARD_NUMBER}_local.csv"
+    ls_file = f"annotation_v1_shard{SHARD_NUMBER}_ls.csv"
+    out_dir = REPO_ROOT / "data" / "meta" / "v1"
+
+    make_ls_csv(shard_local_csv=out_dir / target_file,
+                out_csv=out_dir / ls_file)
