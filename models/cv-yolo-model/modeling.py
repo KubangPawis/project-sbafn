8169840c0c246c97dbdec10de39b1dfb62bcba62
KubangPawis <xanderfernandezperegrin@gmail.com>
2025-10-25 19:24:48 +0800

    feat(yolo): add model export

--- models/cv-yolo-model/modeling.py
+++ models/cv-yolo-model/modeling.py
@@ -12,6 +12,9 @@ CV_MODEL_DIR = MODELS_DIR / "cv-yolo-model"
 with open(MODELS_DIR / "cv-yolo-model" / "configs" / "yolo11s_v1_01.yaml", "r", encoding="utf-8") as f:
     cfg_all = yaml.safe_load(f)
 
+# CHOSEN MODEL
+BEST_MODEL_PATH = REPO_ROOT / "models" / "cv-yolo-model" / "runs" / "y11s_baseline" / "weights" / "best.pt"
+
 # --------------
 
 def run_yolo_model():
@@ -26,6 +29,34 @@ def run_yolo_model():
               project=cfg.get("project"), name=f"{cfg.get('name','run')}_val")
     model.val(data=data, split="test",
                project=cfg.get("project"), name=f"{cfg.get('name','run')}_test")
+    
+def export_model(model_weights_path: Path):
+    best = model_weights_path
+    if not best.exists():
+        raise FileNotFoundError(best)
+
+    # [CONFIGS] Set export configs
+    export_params = dict((cfg_all.get("export") or {}))
+    export_params.pop("enabled", None)
+
+    # Setting defaults (in case not in .yaml)
+    export_params.setdefault("format", "onnx")
+    export_params.setdefault("imgsz", 640)
+    export_params.setdefault("dynamic", True)
+    export_params.setdefault("opset", 12)
+
+    # [EXPORT] Model export based on export configs
+    proj = export_params.get("project")
+    if proj:
+        Path(proj).mkdir(parents=True, exist_ok=True)
+
+    model = YOLO(str(best))
+    result = model.export(**export_params)
+    export_dir = Path(export_params["project"]) / export_params["name"]
+    print("Export dir:", export_dir.resolve())
+
+    return result
 
 if __name__ == "__main__":
-    run_yolo_model()
+    #run_yolo_model()
+    export_model(model_weights_path=BEST_MODEL_PATH)
