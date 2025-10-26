e55df2573ef1a934414b204bfa767bcdc9e337b9
KubangPawis <xanderfernandezperegrin@gmail.com>
2025-10-26 14:07:32 +0800

    chore(frontend): remove test default scaffold

--- sbafn_app/test/widget_test.dart
+++ sbafn_app/test/widget_test.dart
@@ -1,9 +1,1 @@
-import 'package:flutter/material.dart';
-import '../lib/pages/home.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: StoryMapHomePage(),
-  ));
-}
